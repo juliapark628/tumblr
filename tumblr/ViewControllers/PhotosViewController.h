58bddfe2865c80192813bbc7f227757f351ad1e6
Julia Park <juliapark628@fb.com>
2019-06-27 11:43:26 -0700

    finished tumblr mock

---
+++ tumblr/ViewControllers/PhotosViewController.h
@@ -0,0 +1,18 @@
+//
+//  PhotosViewController.h
+//  tumblr
+//
+//  Created by juliapark628 on 6/27/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIImageView+AFNetworking.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotosViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ tumblr/Views/PhotoCell.h
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.h
+//  tumblr
+//
+//  Created by juliapark628 on 6/27/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
