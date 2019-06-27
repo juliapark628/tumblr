77134e177414f91826653ebcb1497f2ca7a4fea9
Julia Park <juliapark628@fb.com>
2019-06-27 10:28:44 -0700

    Initial Commit

---
+++ tumblr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tumblr
+//
+//  Created by juliapark628 on 6/27/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
